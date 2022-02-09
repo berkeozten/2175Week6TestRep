31e4e8966d84aaf21b824b48c3c42b59c10a5327
300346658 <300346658@R5107S19.ad.douglascollege.ca>
2022-02-08 18:13:01 -0800

    Initial check

---
+++ Class6/src/EmployeeDemo.java
@@ -0,0 +1,17 @@
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		Employee [] emps = new Employee[4];
+		emps[0] = new Employee(1001,5000.0);
+		*/
+		Employee [] emps = {new Employee(1002,1007.0),new Employee(),new Employee(),new Employee()};
+		
+		for(Employee emp:emps) {
+			System.out.println(emp);
+		}
+	}
+
+}
